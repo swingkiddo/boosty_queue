1948b627c1c55eba670e80ed601cd7c9e19ef20d
koocha <koochamala@gmail.com>
2025-06-01 10:04:56 +0300

    feat: Enhance data models, introduce SessionReview and base model utilities

--- bot/models/session.py
+++ bot/models/session.py
@@ -1,5 +1,5 @@
 from .base import Base
-from sqlalchemy import Column, String, BigInteger, DateTime, Integer, Float, ForeignKey, Enum
+from sqlalchemy import Column, String, BigInteger, DateTime, Integer, Float, ForeignKey, Enum, Boolean
 from sqlalchemy.orm import relationship
 import enum
 
@@ -7,15 +7,18 @@ class Session(Base):
     __tablename__ = "sessions"
 
     id = Column(Integer, primary_key=True)
+    type = Column(String, nullable=False)
     coach_id = Column(BigInteger, ForeignKey("users.id"), nullable=False)
     date = Column(DateTime, nullable=False)
-    voice_channel_id = Column(BigInteger, nullable=False)
-    text_channel_id = Column(BigInteger, nullable=False)
-    info_message_id = Column(BigInteger, nullable=False)
-    planned_start_time = Column(DateTime, nullable=False)
-    end_time = Column(DateTime, nullable=False)
+    voice_channel_id = Column(BigInteger, nullable=True)
+    text_channel_id = Column(BigInteger, nullable=True)
+    info_message_id = Column(BigInteger, nullable=True)
+    session_message_id = Column(BigInteger, nullable=True)
+    start_time = Column(DateTime, nullable=True, )
+    end_time = Column(DateTime, nullable=True)
     max_slots = Column(Integer, default=8)
     requests = relationship("SessionRequest", back_populates="session")
+    is_active = Column(Boolean, default=False)
 
 class SessionRequestStatus(enum.Enum):
     PENDING = "pending"
@@ -29,5 +32,14 @@ class SessionRequest(Base):
     id = Column(Integer, primary_key=True)
     session_id = Column(Integer, ForeignKey("sessions.id"), nullable=False)
     user_id = Column(BigInteger, ForeignKey("users.id"), nullable=False)
-    status = Column(Enum(SessionRequestStatus), nullable=False)
+    status = Column(String, nullable=False)
     session = relationship("Session", back_populates="requests")
+    user = relationship("User", back_populates="session_requests")
+
+class SessionReview(Base):
+    __tablename__ = "session_reviews"
+
+    id = Column(Integer, primary_key=True)
+    session_id = Column(Integer, ForeignKey("sessions.id"), nullable=False)
+    user_id = Column(BigInteger, ForeignKey("users.id"), nullable=False)
+    rating = Column(Integer, nullable=False)
--- bot/models/user.py
+++ bot/models/user.py
@@ -1,13 +1,15 @@
 from .base import Base
 from sqlalchemy import Column, String, BigInteger, DateTime, Integer, Float
+from sqlalchemy.orm import relationship
 
 class User(Base):
     __tablename__ = "users"
     id = Column(BigInteger, primary_key=True)
     nickname = Column(String, nullable=False)
     join_date = Column(DateTime, nullable=False)
-    total_replays_session = Column(Integer, default=0)
+    total_replay_sessions = Column(Integer, default=0)
     total_creative_sessions = Column(Integer, default=0)
     priority_coefficient = Column(Float, default=0)
-    priority_given_by = Column(BigInteger, nullable=False)
-    priority_expires_at = Column(DateTime, nullable=False)
+    priority_given_by = Column(BigInteger, nullable=True)
+    priority_expires_at = Column(DateTime, nullable=True)
+    session_requests = relationship("SessionRequest", back_populates="user")
